ef7a0f07cb2b29769e1c0328bdc5e0d57af28212
Matt Hadden <forceh2010@gmail.com>
2025-06-07 21:20:15 -0400

    refactor(cdrom): pass buffer itself out instead of a copy/re-alloc

--- src/helpers/cdrom.cpp
+++ src/helpers/cdrom.cpp
@@ -13,7 +13,7 @@ void CDRomHelper::init()
 
 // reads a file off of the cd rom into memory (most likely a file full of binary data)
 // dont forget to free the returned data when you're done with it
-void CDRomHelper::load_file(const char *file_name, eastl::function<void(void *, size_t)> callback)
+void CDRomHelper::load_file(const char *file_name, eastl::function<void(psyqo::Buffer<uint8_t>)> callback)
 {
     printf("CD ROM: Attempting to read %s...\n", file_name);
 
@@ -21,33 +21,18 @@ void CDRomHelper::load_file(const char *file_name, eastl::function<void(void *, 
     m_cdromLoader.readFile(file_name, m_isoParser,
                            [file_name, callback](psyqo::Buffer<uint8_t> &&buffer)
                            {
-                               printf("CD ROM: Finished reading file from cd rom.\n");
-                               psyqo::Buffer<uint8_t> local_buffer = eastl::move(buffer);
+                               printf("CD ROM: Finished reading file (%d bytes) from cd rom.\n", buffer.size());
 
                                // the file isnt there or its empty
-                               if (local_buffer.empty())
+                               if (buffer.empty())
                                {
+                                   buffer.clear();
                                    printf("CD ROM: File %s not found on CD or file is empty\n", file_name);
                                    return;
                                }
 
-                               // allocate the ptr
-                               size_t size = local_buffer.size();
-                               void *ptr = psyqo_malloc(size);
-                               if (!ptr)
-                               {
-                                   printf("CD ROM: Failed to malloc\n");
-                                   return;
-                               }
-
-                               // return the data (dont forget to free it when you're done with it!)
-                               __builtin_memcpy(ptr, local_buffer.data(), size);
-
                                // callback to the loader
-                               callback(ptr, local_buffer.size());
-
-                               // clear the local buffer
-                               local_buffer.clear();
+                               callback(eastl::move(buffer));
                            });
 }
 
--- src/mesh/mesh_manager.cpp
+++ src/mesh/mesh_manager.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "mesh_manager.hh"
 #include "../helpers/cdrom.hh"
 
@@ -197,3 +198,206 @@ void MeshManager::unload_mesh(const char *mesh_name)
         }
     }
 }
+=======
+#include "mesh_manager.hh"
+#include "../helpers/cdrom.hh"
+
+LOADED_MESH MeshManager::m_loaded_meshes[MAX_CDROM_FILE_NAME_LEN];
+
+void MeshManager::load_mesh_from_cdrom(const char *mesh_name, eastl::function<void(MESH *mesh_out)> onComplete)
+{
+    // is it already loaded?
+    MESH *p_mesh = is_mesh_loaded(mesh_name);
+    if (p_mesh != nullptr)
+    {
+        onComplete(p_mesh);
+        return;
+    }
+
+    // is there space for this mesh?
+    int8_t mesh_ix = find_space_for_mesh();
+    if (mesh_ix == -1)
+    {
+        onComplete(nullptr);
+        return;
+    }
+
+    // try and load the mesh off the cdrom
+    char mesh_file_name[MAX_CDROM_FILE_NAME_LEN];
+    CDRomHelper::get_iso_file_name(mesh_name, mesh_file_name);
+    CDRomHelper::load_file(mesh_file_name, [mesh_name, mesh_ix, onComplete](psyqo::Buffer<uint8_t> buffer)
+                           {
+                            void * data = buffer.data();
+                            size_t size = buffer.size();
+        if (data == nullptr || size == 0)
+        {
+            printf("MESH: Failed to load mesh or it has no file size.\n");
+            return;
+        }
+
+        // basic struct setup
+        LOADED_MESH loaded_mesh = {0};
+        loaded_mesh.mesh_name = mesh_name;
+        __builtin_memset(&loaded_mesh.mesh, 0, sizeof(MESH));
+
+        // get ready with our buffer
+        unsigned char* ptr = (unsigned char*)data;
+
+        // read the model file header, which is the vertex/indices/face count
+        __builtin_memcpy(&loaded_mesh.mesh.vertex_count, ptr, 4);
+        ptr += 4;
+
+        __builtin_memcpy(&loaded_mesh.mesh.indices_count, ptr, 4);
+        ptr += 4;
+
+        __builtin_memcpy(&loaded_mesh.mesh.faces_num, ptr, 4);
+        ptr += 4;
+
+        // do we have too many faces?
+        if (loaded_mesh.mesh.faces_num >= MAX_FACES_PER_MESH) {
+            printf("MESH: Mesh has too many faces, aborting load.\n");
+            __builtin_memset(&loaded_mesh, 0, sizeof(LOADED_MESH));
+            onComplete(nullptr);
+            psyqo_free(data);
+            return;
+        }
+
+        // read the verts
+        size_t vertices_size = sizeof(psyqo::Vec3) * loaded_mesh.mesh.vertex_count;
+        loaded_mesh.mesh.vertices = (psyqo::Vec3 *)psyqo_malloc(vertices_size);
+
+        int32_t x, y, z;
+        for (int i = 0; i < loaded_mesh.mesh.vertex_count; i++) {
+            __builtin_memcpy(&x, ptr, sizeof(int32_t));
+            ptr += sizeof(int32_t);
+
+            __builtin_memcpy(&y, ptr, sizeof(int32_t));
+            ptr += sizeof(int32_t);
+
+            __builtin_memcpy(&z, ptr, sizeof(int32_t));
+            ptr += sizeof(int32_t);
+
+            loaded_mesh.mesh.vertices[i].x.value = x;
+            loaded_mesh.mesh.vertices[i].y.value = y;
+            loaded_mesh.mesh.vertices[i].z.value = z;
+        }
+
+        // read the verts paint data
+        size_t vertices_paint_size = sizeof(VERTEX_PAINT) * loaded_mesh.mesh.vertex_count;
+        loaded_mesh.mesh.vertex_paint = (VERTEX_PAINT *)psyqo_malloc(vertices_paint_size);
+        __builtin_memcpy(loaded_mesh.mesh.vertex_paint, ptr, vertices_paint_size);
+        ptr += vertices_paint_size;
+
+        // read the verts indices
+        size_t indices_size = sizeof(INDEX) * loaded_mesh.mesh.indices_count;
+        loaded_mesh.mesh.indices = (INDEX *)psyqo_malloc(indices_size);
+        __builtin_memcpy(loaded_mesh.mesh.indices, ptr, indices_size);
+        ptr += indices_size;
+
+        // read the normals count
+        int32_t normal_count;
+        __builtin_memcpy(&normal_count, ptr, 4);
+        ptr += 4;
+
+        // read the normals data
+        size_t normals_size = sizeof(psyqo::Vec3) * normal_count;
+        loaded_mesh.mesh.normals = (psyqo::Vec3 *)psyqo_malloc(normals_size);
+
+        for (int i = 0; i < normal_count; i++) {
+            __builtin_memcpy(&x, ptr, sizeof(int32_t));
+            ptr += sizeof(int32_t);
+
+            __builtin_memcpy(&y, ptr, sizeof(int32_t));
+            ptr += sizeof(int32_t);
+
+            __builtin_memcpy(&z, ptr, sizeof(int32_t));
+            ptr += sizeof(int32_t);
+
+            loaded_mesh.mesh.normals[i].x.value = x;
+            loaded_mesh.mesh.normals[i].y.value = y;
+            loaded_mesh.mesh.normals[i].z.value = z;
+        }
+
+        // read the UV count
+        size_t uv_count;
+        __builtin_memcpy(&uv_count, ptr, 4);
+        ptr+= 4;
+
+        // read the uv data
+        size_t uvs_size = sizeof(UV)*uv_count;
+        loaded_mesh.mesh.uvs = (UV *)psyqo_malloc(uvs_size);
+        __builtin_memcpy(loaded_mesh.mesh.uvs, ptr, uvs_size);
+        ptr+= uvs_size;
+
+        // read the uv indices
+        size_t uv_indices_size = sizeof(INDEX)*loaded_mesh.mesh.indices_count;
+        loaded_mesh.mesh.uv_indices = (INDEX *)psyqo_malloc(uv_indices_size);
+        __builtin_memcpy(loaded_mesh.mesh.uv_indices, ptr, uv_indices_size);
+        ptr += uv_indices_size;
+
+        // mark mesh as loaded
+        loaded_mesh.is_loaded = true;
+
+        // store in loaded meshes
+        m_loaded_meshes[mesh_ix] = loaded_mesh;
+
+        // free the data
+        buffer.clear();
+
+        printf("MESH: Successfully loaded mesh of %d bytes into memory.\n", size);
+
+        // callback to whatever asked to load the file
+        onComplete(&m_loaded_meshes[mesh_ix].mesh); });
+}
+
+MESH *MeshManager::is_mesh_loaded(const char *mesh_name)
+{
+    using FixedString = eastl::fixed_string<char, MAX_CDROM_FILE_NAME_LEN>;
+    FixedString eastl_mesh_name(mesh_name);
+
+    LOADED_MESH *loaded_mesh = nullptr;
+    for (int i = 0; i < MAX_LOADED_MESHES; i++)
+    {
+        // find the first loaded mesh that matches this mesh_name
+        loaded_mesh = &m_loaded_meshes[i];
+        if (loaded_mesh && eastl_mesh_name == FixedString(loaded_mesh->mesh_name))
+        {
+            return &loaded_mesh->mesh;
+        }
+    }
+
+    // can't find a mesh with this file name
+    return nullptr;
+}
+
+int8_t MeshManager::find_space_for_mesh(void)
+{
+    for (int8_t i = 0; i < MAX_LOADED_MESHES; i++)
+    {
+        // return the first mesh that isn't loaded
+        if (m_loaded_meshes->is_loaded == false)
+            return i;
+    }
+
+    // no space in the mesh manager for it
+    return -1;
+}
+
+void MeshManager::unload_mesh(const char *mesh_name)
+{
+    using FixedString = eastl::fixed_string<char, MAX_CDROM_FILE_NAME_LEN>;
+    FixedString eastl_mesh_name(mesh_name);
+
+    LOADED_MESH *loaded_mesh = nullptr;
+    for (int i = 0; i < MAX_LOADED_MESHES; i++)
+    {
+        // find the first loaded mesh that matches this mesh_name
+        loaded_mesh = &m_loaded_meshes[i];
+        if (loaded_mesh && eastl_mesh_name == FixedString(loaded_mesh->mesh_name))
+        {
+            __builtin_memset(loaded_mesh, 0, sizeof(LOADED_MESH));
+            break;
+        }
+    }
+}
+>>>>>>> d71c1f3 (refactor(cdrom): pass buffer itself out instead of a copy/re-alloc)
